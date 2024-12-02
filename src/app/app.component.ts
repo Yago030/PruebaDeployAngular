7307f653ede8f18192b86b22c238a51820ca014f
Santiago Bergerat <santiagobergerat@gmail.com>
2024-12-02 12:08:42 -0300

    subiendo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}}   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia, iusto. Rerum qui cupiditate ratione doloribus sunt reiciendis repellendus quia, expedita sit quae dolorum inventore nesciunt culpa! Sit consectetur exercitationem blanditiis.</div>`,
 })
 export class AppComponent {
   value = 'World';
